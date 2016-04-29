fbca5627230bf9094fa8789437c23944d737d2d3
Arsalan Sufi <arsalan.sufi@yale.edu>
2016-04-29 00:33:52 -0400

    Add script to test loading with indexes added before and after

---
+++ scripts/tests/sql/add-edges-indexes.sql
@@ -0,0 +1,2 @@
+ALTER TABLE ONLY edges
+    ADD CONSTRAINT edges_pkey PRIMARY KEY (node_1, node_2);
---
+++ scripts/tests/sql/setup-edges-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE edges (
+    node_1 integer NOT NULL,
+    node_2 integer NOT NULL,
+    "timestamp" timestamp without time zone
+);
